c15db79f892b0e748a3af2dbe0b2273b19144604
swadhi03 <swathiksanthosh03@gmail.com>
2024-06-07 21:47:41 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import AddPhoto from './components/AddPhoto';
+import SearchPhoto from './components/SearchPhoto';
+import DeletePhoto from './components/DeletePhoto';
+import ViewAll from './components/ViewAll';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AddPhoto/>}/>
+        <Route path='search/' element={<SearchPhoto/>}/>
+        <Route path='delete/' element={<DeletePhoto/>}/>
+        <Route path='view/' element={<ViewAll/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/DeletePhoto.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const DeletePhoto = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row">
+                        <div className="col col-12 col-sm-12 col-md-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="foem-label">Photo Title</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-danger">Delete</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default DeletePhoto
